8da6ff21f05ca785b830497175f6aa56d45579f5
RachRaja <85390.rajasekaran@students.itu.edu>
2014-11-17 09:02:16 -0800

    Create Hypergraphs.java

---
+++ Hypergraphs.java
@@ -0,0 +1,21 @@
+package edu.uci.ics.jung.graph;
+public class Hypergraph {
+	
+	public String Vertex;
+	public String Edge; 
+	
+	public void Hyperclass (String addEdge, String addVertex)
+	{
+		Edge = addEdge;
+		Vertex = addVertex;
+		}
+	public String getEdge()
+	{
+		return Edge;}
+		public void setEdge(String newValue){
+			Edge = newValue;
+		}
+		  public static void main(String[] args){
+	
+		  }
+}
